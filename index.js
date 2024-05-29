fdeeb4d0e9249a0437c7374e1b08b29c1bae0544
Suphawit Buaphan <122143843+SSS-BBB@users.noreply.github.com>
2024-05-29 15:08:29 +0700

    remove bot function

--- index.js
+++ index.js
@@ -19,6 +19,20 @@ controlBot.on("chat", (username, message) => {
             createBots(amount)
         }
     }
+
+    if(message.toLowerCase() === "remove all") {
+        let amount = allBots.length
+        controlBot.chat("Removing " + amount + " bots...")
+        removeAllBots()
+    }
+
+    else if (message.split(" ")[0].toLowerCase() === "remove") {
+        let id = parseInt(message.split(" ")[1])
+        if (id) {
+            controlBot.chat("Removing " + id)
+            removeBotByID(id)
+        }
+    }
 })
 
 function createBots(amount) {
@@ -40,4 +54,37 @@ function createBots(amount) {
 
         id++
     }
+}
+
+function removeAllBots() {
+    // console.log(`Before remove: ${allBots}`)
+
+    let amount = allBots.length
+    for (let i = 0; i < amount; i++) {
+        allBots[i].quit()
+        controlBot.chat(`${allBots[i].username} is removed.`)
+        allBots[i] = ""
+    }
+    allBots = allBots.filter( (b) => b !== "" )
+
+    controlBot.chat(`${amount} bots has been removed.`)
+
+    // console.log(`After remove: ${allBots}`)
+}
+
+function removeBotByID(id) {
+    // console.log(`Before remove: ${allBots}`)
+
+    const botName = `ID_${id}`
+    const bot = allBots.find( (b) => b.username === botName )
+    if (bot) {
+        bot.quit()
+        allBots = allBots.filter( (b) => b.username !== botName )
+        controlBot.chat(`${botName} has been removed.`)
+    }
+    else {
+        controlBot.chat(`${botName} not found.`)
+    }
+
+    // console.log(`After remove: ${allBots}`)
 }
