9375ed83674c83425a75144357741831ca17836d
Suphawit Buaphan <122143843+SSS-BBB@users.noreply.github.com>
2024-05-29 21:28:55 +0700

    create mcBot class

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mineflayer = require("mineflayer")
+const { MCBot } = require("./mcBot")
 
-const PORT = 63230
+const PORT = 61126
 
 let id = 1
 let allBots = []
@@ -11,7 +12,7 @@ const controlBot = mineflayer.createBot({
     username: "Control_Bot"
 })
 
-controlBot.on("chat", (username, message) => {
+controlBot.on("chat", async (username, message) => {
     if (message.split(" ")[0].toLowerCase() === "create") {
         let amount = parseInt(message.split(" ")[1])
         if (amount) {
@@ -33,6 +34,48 @@ controlBot.on("chat", (username, message) => {
             removeBotByID(id)
         }
     }
+
+    if (message.split(" ")[0].toLowerCase() === "observe") {
+        let id = parseInt(message.split(" ")[1])
+        if (id) {
+            controlBot.chat("Observation of " + id)
+            obs = getObservations(id)
+            if (obs) {
+                obs.forEach(e => {
+                    controlBot.chat(String(e))
+                })
+            }
+        }
+    }
+
+    if (message.split(" ")[0].toLowerCase() === "randomact") {
+        let id = parseInt(message.split(" ")[1])
+        if (id) {
+            await randomAction(id)
+        }
+    }
+
+    if (message.split(" ")[0].toLowerCase() === "hit") {
+        let id = parseInt(message.split(" ")[1])
+
+        // Get Bot
+        const botClass = getBotFromID(id)
+        if (!botClass) {
+            return
+        }
+
+        botClass.botAction(7)
+    }
+
+    if (message.split(" ")[0].toLowerCase() === "tp") {
+        let id = parseInt(message.split(" ")[1])
+
+        // Get Bot
+        const botClass = getBotFromID(id)
+        if (botClass) {
+            controlBot.chat(`/tp ${botClass.name} -207 -60 180`)
+        }
+    }
 })
 
 function createBots(amount) {
@@ -40,14 +83,10 @@ function createBots(amount) {
 
         // Create new bot
         const botName = `ID_${id}`
-        const bot = mineflayer.createBot({
-            host: "localhost",
-            port: PORT,
-            username: botName
-        })
+        const botClass = new MCBot(botName, PORT)
         
         // Add bot to list
-        allBots.push(bot)
+        allBots.push(botClass)
 
         // Control bot chat
         controlBot.chat(botName + " Created.")
@@ -56,13 +95,26 @@ function createBots(amount) {
     }
 }
 
+function getBotFromID(id) {
+    // Get Bot
+    const botName = `ID_${id}`
+    const botClass = allBots.find( (b) => b.name === botName )
+
+    if (!botClass) {
+        controlBot.chat(`${botName} not found.`)
+        return false
+    }
+
+    return botClass
+}
+
 function removeAllBots() {
     // console.log(`Before remove: ${allBots}`)
 
     let amount = allBots.length
     for (let i = 0; i < amount; i++) {
-        allBots[i].quit()
-        controlBot.chat(`${allBots[i].username} is removed.`)
+        allBots[i].bot.quit()
+        controlBot.chat(`${allBots[i].name} is removed.`)
         allBots[i] = ""
     }
     allBots = allBots.filter( (b) => b !== "" )
@@ -76,15 +128,33 @@ function removeBotByID(id) {
     // console.log(`Before remove: ${allBots}`)
 
     const botName = `ID_${id}`
-    const bot = allBots.find( (b) => b.username === botName )
-    if (bot) {
-        bot.quit()
-        allBots = allBots.filter( (b) => b.username !== botName )
+    const botClass = getBotFromID(id)
+    if (botClass) {
+        botClass.bot.quit()
+        allBots = allBots.filter( (b) => b.name !== botName )
         controlBot.chat(`${botName} has been removed.`)
-    }
-    else {
-        controlBot.chat(`${botName} not found.`)
     }
 
     // console.log(`After remove: ${allBots}`)
+}
+
+async function randomAction(id) {
+    // Get Bot
+    const botClass = getBotFromID(id)
+    if (!botClass) {
+        return
+    }
+
+    // Random action
+    const randAction = Math.floor(Math.random() * 8)
+    await botClass.botAction(randAction)
+}
+
+function setBotToArena(id) {
+    const botClass = getBotFromID(id)
+    if (botClass) {
+        const randX = -210 + Math.floor(Math.random() * (-203 + 210 + 1))
+        const randZ = -210 + Math.floor(Math.random() * (-203 + 210 + 1))
+        controlBot.chat(`/tp ${botClass.name} -207 -60 180`)
+    }
 }
---
+++ mcBot.js
@@ -0,0 +1,66 @@
+const mineflayer = require("mineflayer")
+
+exports.MCBot = class {
+    constructor(name, port) {
+        this.bot = mineflayer.createBot({
+            host: "localhost",
+            port: port,
+            username: name
+        })
+
+        this.name = name
+        this.port = port
+    }
+
+    getObservations() {
+        // Find nearest bot or player
+        const nearestEntity = this.bot.nearestEntity( (e) => e.name.includes("ID") || 
+        e.type === "player" || 
+        e.type === "hostile" )
+
+        // [ myPos, otherPos, myHealth ]
+        return [ 
+            this.bot.entity.position.x,
+            this.bot.entity.position.y,
+            this.bot.entity.position.z,
+
+            nearestEntity.position.x,
+            nearestEntity.position.y,
+            nearestEntity.position.z,
+
+            this.bot.health
+        ]
+    }
+
+    async botAction(actionId) {
+        // 0 -> forward, 1 -> back, 2 -> left, 3 -> right, 4 -> jump, 5 -> sprint, 6 -> sneak, 7 -> hit
+        const actionSize = 8
+        if (actionId + 1 > actionSize) {
+            this.bot.chat(`No ${actionId} action.`)
+            return false
+        }
+    
+        // Reset control state
+        const controlStateList = ["forward", "back", "left", "right", "jump", "sprint", "sneak"]
+        controlStateList.forEach( (action) => {
+            this.bot.setControlState(action, false)
+        } )
+    
+        // Action
+        if (actionId < 7) {
+            this.bot.setControlState(controlStateList[actionId], true)
+            this.bot.chat(controlStateList[actionId] + "!")
+        }
+        else {
+            // Hit
+            // Find nearest bot or player
+            const nearestEntity = this.bot.nearestEntity( (e) => e.name.includes("ID") || 
+            e.type === "player" || 
+            e.type === "hostile" )
+    
+            await this.bot.lookAt(nearestEntity.position.offset(0, nearestEntity.height, 0))
+            this.bot.attack(nearestEntity)
+            this.bot.chat("Hit!")
+        }
+    }
+}
